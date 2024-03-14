6461d7018fa4e7f00bcc4a18c09cdd65b34f25e2
Kshitij <kshitijguladhe2002@gmail.com>
2024-03-14 21:27:12 +0530

    Some New Changes

--- main.js
+++ main.js
@@ -7,8 +7,35 @@ menu.addEventListener('click',function(){
 })
 close.addEventListener('click',function(){
     document.getElementById('menu').style.right = '-60vmax';
-
 })
+
+// script.js
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }``
+  
+
+var tl = gsap.timeline();
+function loading(){
+    
+tl.to(".loader",{
+    top:"-100%",
+    delay:0.5,
+    duration:3,
+    ease:"expo.out"
+})
+}
+loading();
 
 var typed = new Typed(".text", {
     strings: ["Frontend developer", "Youtuber", "Web Developer"],
